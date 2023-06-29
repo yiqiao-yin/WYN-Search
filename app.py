416a3db5df7ea1b93c056332b2c3a2508beb8098
Yiqiao Yin <eagle0504@gmail.com>
2023-06-29 01:11:31 -0400

    initial push

---
+++ app.py
@@ -0,0 +1,43 @@
+import google.generativeai as palm
+import streamlit as st
+
+palm_api_key = st.secrets["PALM_API_KEY"]
+palm.configure(api_key=palm_api_key)
+
+def call_palm(prompt: str) -> str:
+    completion = palm.generate_text(
+        model="models/text-bison-001",
+        prompt=prompt,
+        temperature=0,
+        max_output_tokens=800,
+    )
+    return completion.result
+
+
+st.title("WYN Search 🧐")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # Get response
+    response = call_palm(f"{prompt}")
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
